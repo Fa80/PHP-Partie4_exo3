2def96a24767dcdbbec80e42c717f89a335f6ba3
Fa <ngomfa@gmail.com>
2018-11-26 15:23:19 +0100

    exo3

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>Partie4_Exercice 3</title>
+</head>
+<!-- Faire une fonction qui prend en paramètre deux chaines de caractères et qui revoit la concaténation de ces deux chaines -->
+<body>
+<?php
+function string($name = 'LA GLOBULE'){
+return 'Bonjour'. ' '.$name.'!';
+}
+?>
+<?php
+echo string ();
+?>
+</body>
+</html>
